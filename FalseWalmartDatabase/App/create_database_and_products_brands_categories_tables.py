80a006bdb6a0dacf532628d8e1e9324adb11f367
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-25 14:53:21 -0600

    Products table creation

--- FalseWalmartDatabase/App/create_database_and_products_brands_categories_tables.py
+++ FalseWalmartDatabase/App/create_database_and_products_brands_categories_tables.py
@@ -21,6 +21,32 @@ def create_brands_table() -> None:
     except Exception as e:
         print(f'Impossible to execute the query:{e}')
 
+# Create products table
+def create_products_table() -> None:
+    try:
+        db.execute_query("""CREATE TABLE IF NOT EXISTS Products (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                        name VARCHAR(150) NOT NULL, 
+                        description TEXT DEFAULT '', 
+                        cost_price REAL NOT NULL, 
+                        selling_price REAL NOT NULL,
+                        revenue REAL DEFAULT 0,
+                        category_id INTEGER NOT NULL,
+                        brand_id INTEGET NOT NULL,
+                        quantity REAL NOT NULL,
+                        created_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
+                        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                        FOREIGN KEY (category_id) REFERENCES Categories(id),
+                        FOREIGN KEY (brand_id) REFERENCES Brands(id)
+                        );""")
+        db.database.commit()
+        print('Products table: Created | Already exists!')
+    except Exception as e:
+        print(f'Impossible to execute the query:{e}')
+
+# Create tables
+create_products_table()
+
 # Select existing table names
 query = "SELECT NAME FROM sqlite_master WHERE type='table'"
 q_result = db.execute_query(query)
